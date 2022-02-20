163e1aaccbffa67dd129ddce7b30d315590d5157
Rahul Vig <rahulvig513@gmail.com>
2022-02-20 13:31:31 -0600

    align with test

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   resolve: {
-    alias: [
-      { find: '~', replacement: path.resolve(__dirname, 'src') },
-    ],
+    alias: {
+      'src': path.resolve(__dirname, './src'),
+    },
   },
-  plugins: [react()]
 })
