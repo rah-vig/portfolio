63e8f0fbbfed7c47233af201d71cc2a55bb23690
Rahul Vig <rahulvig513@gmail.com>
2022-02-20 08:24:08 -0600

    initial section boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
+import Skills from "./sections/Skills";
+import Home from "./sections/Home";
+import Experience from "./sections/Experience";
+import About from "./sections/About";
+import Intro from "./sections/Intro";
+import Projects from "./sections/Projects";
+import Tutorials from "./sections/Tutorials";
+import Contact from "./sections/Contact";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  )
+    return (
+        <div className="App">
+            <Intro/>
+            <Home/>
+            <Skills/>
+            <Projects/>
+            <Experience/>
+            <About/>
+            <Tutorials/>
+            <Contact/>
+        </div>
+    )
 }
 
 export default App
