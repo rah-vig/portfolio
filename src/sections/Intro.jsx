67a06b802751927f96c869d4c7c45e9ff2a85413
Rahul Vig <rahulvig513@gmail.com>
2022-02-21 17:43:45 -0600

    refactoring

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -1,35 +1,16 @@
 import React from 'react';
 import {Parallax} from "react-scroll-parallax";
-import {PARTICLE_OPTIONS} from "src/section-configs/IntroConfig";
+import {PARTICLE_OPTIONS, ANIMATED_INTRO_SENTENCES} from "src/section-configs/IntroConfig";
 import Particles from "react-tsparticles";
 import AnimateListElements from "../utilities/AnimateListElements";
 
 function Intro(props) {
-    const sentences = ["Hey there!", "I'm Rahul", <div>I am a Software Developer specializing in web development</div>]
-
-    const particlesInit = (main) => {
-        console.log(main)
-
-        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    };
-
-    const particlesLoaded = (container) => {
-        console.log(container);
-    };
-
     return (
         <Parallax className="sections intro">
-            <Particles
-                id="tsparticles"
-                init={particlesInit}
-                loaded={particlesLoaded}
-                options={PARTICLE_OPTIONS}
-            />
-
+            <Particles id="tsparticles" options={PARTICLE_OPTIONS}/>
             <div className="content">
-                <AnimateListElements transitionElements={sentences}/>
+                <AnimateListElements transitionElements={ANIMATED_INTRO_SENTENCES}/>
             </div>
-
         </Parallax>
     );
 }
