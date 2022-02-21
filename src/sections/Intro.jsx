f7609cb819c0bf517d379bf971bf0e98e8e839e1
Rahul Vig <rahulvig513@gmail.com>
2022-02-21 15:27:59 -0600

    refactor rename

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Parallax} from "react-scroll-parallax";
 import {PARTICLE_OPTIONS} from "src/section-configs/IntroConfig";
 import Particles from "react-tsparticles";
-import AnimatedPhrase from "../utilities/AnimatedPhrase";
+import AnimateListElements from "../utilities/AnimateListElements";
 
 function Intro(props) {
     const sentences = ["Hey there!", "I'm Rahul", <div>I am a Software Developer specializing in web development</div>]
@@ -27,7 +27,7 @@ function Intro(props) {
             />
 
             <div className="content">
-                <AnimatedPhrase transitionElements={sentences}/>
+                <AnimateListElements transitionElements={sentences}/>
             </div>
 
         </Parallax>
