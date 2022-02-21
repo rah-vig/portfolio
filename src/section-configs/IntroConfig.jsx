ae7ec289b9555d7e0a031fbe6bc3a42f7c6ca993
Rahul Vig <rahulvig513@gmail.com>
2022-02-21 17:43:21 -0600

    chg ext to jsx

---
+++ src/section-configs/IntroConfig.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+const PARTICLE_OPTIONS = {
+    "fullScreen": {
+        "enable": true, "zIndex": 100
+    }, "interactivity": {
+        "events": {
+            "onClick": {
+                "enable": true, "mode": "repulse"
+            }, "onHover": {
+                "enable": true, "mode": "attract", "parallax": {
+                    "enable": true, "force": 10
+                }
+            }
+        }, "modes": {
+            "attract": {
+                "distance": 500
+            }, "bubble": {
+                "distance": 250, "duration": 2, "opacity": 0, "size": 0
+            }, "grab": {
+                "distance": 400
+            }, "repulse": {
+                "distance": 400
+            }
+        }
+    }, "motion": {
+        "reduce": {
+            "factor": 10
+        }
+    }, "particles": {
+        "color": {
+            "value": "#008A64"
+        }, "links": {
+            "color": {
+                "value": "#ffffff"
+            }, "distance": 150, "opacity": 0.4
+        }, "move": {
+            "attract": {
+                "rotate": {
+                    "x": 600, "y": 600
+                }
+            }, "enable": true, "path": {}, "outModes": {
+                "bottom": "out", "left": "out", "right": "out", "top": "out"
+            }, "random": true, "speed": 1, "spin": {}
+        }, "number": {
+            "density": {
+                "enable": true
+            }, "value": 160
+        }, "opacity": {
+            "random": {
+                "enable": true
+            }, "value": {
+                "min": 0, "max": 1
+            }, "animation": {
+                "enable": true, "speed": 1, "minimumValue": 0
+            }
+        }, "shadow": {
+            "blur": 0.6, "enable": true, "offset": {
+                "x": 1, "y": 1
+            }
+        }, "size": {
+            "random": {
+                "enable": true
+            }, "value": {
+                "min": 1, "max": 3
+            }, "animation": {
+                "speed": 4, "minimumValue": 0.3
+            }
+        }, "twinkle": {
+            "particles": {
+                "frequency": 0.7, "opacity": 0.8
+            }
+        }
+    }
+}
+
+const ANIMATED_INTRO_SENTENCES = [
+    <span>Hey there! </span>,
+    <span>I'm Rahul</span>,
+    <span>I am a Full-Stack Engineer specializing in Web Development</span>
+]
+
+export {PARTICLE_OPTIONS, ANIMATED_INTRO_SENTENCES}
+
+
+
+
+
+
+
