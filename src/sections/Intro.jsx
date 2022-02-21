4f416b355c24360f2cd382c9d26fab130c99d207
Rahul Vig <rahulvig513@gmail.com>
2022-02-21 15:06:57 -0600

    update sections stylesheet

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import {Parallax} from "react-scroll-parallax";
 import {PARTICLE_OPTIONS} from "src/section-configs/IntroConfig";
+import Particles from "react-tsparticles";
+import AnimatedPhrase from "../utilities/AnimatedPhrase";
 
 function Intro(props) {
-    console.log(PARTICLE_OPTIONS)
+    const sentences = ["Hey there!", "I'm Rahul", <div>I am a Software Developer specializing in web development</div>]
+
+    const particlesInit = (main) => {
+        console.log(main)
+
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    };
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
     return (
-        <Parallax className="sections intro" translateY={[-40, 40]}>
-            <div style={{position: "sticky", top: "0"}}>Intro</div>
+        <Parallax className="sections intro">
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={PARTICLE_OPTIONS}
+            />
+
+            <div className="content">
+                <AnimatedPhrase transitionElements={sentences}/>
+            </div>
+
         </Parallax>
     );
 }
